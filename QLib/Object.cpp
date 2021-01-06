fba06a219dfb1b1864665d947162fb7c687eaf50
Q_uan <quan_qixian@163.com>
2021-01-06 07:51:44 -0800

    添加顶层父类Object的实现

---
+++ QLib/Object.cpp
@@ -0,0 +1,32 @@
+#include "Object.h"
+#include <cstdlib>
+
+namespace QLib
+{
+
+void * Object::operator new(unsigned long int size) throw()
+{
+    return malloc(size);
+}
+
+void * Object::operator new[](unsigned long int size) throw()
+{
+    return malloc(size);
+}
+
+void Object::operator delete(void *p)
+{
+    free(p);
+}
+
+void Object::operator delete[](void *p)
+{
+    free(p);
+}
+
+Object:: ~Object()
+{
+
+}
+
+}
---
+++ QLib/Object.h
@@ -0,0 +1,20 @@
+#ifndef OBJECT_H_
+#define OBJECT_H_
+
+namespace QLib
+{
+
+class Object
+{
+public:
+    void * operator new(unsigned long int size) throw();  /* throw()表示当前的函数不会抛出任何异常 */
+    void * operator new[](unsigned long int size) throw();
+
+    void operator delete(void *p);
+    void operator delete[](void *p);
+
+    virtual ~Object() = 0;
+};
+
+}
+#endif
