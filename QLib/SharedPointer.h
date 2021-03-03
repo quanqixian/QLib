5a7c72d922419b03f4d44e52ad828965a159c60d
Q_uan <quan_qixian@163.com>
2021-03-03 07:47:30 -0800

    Add SharedPointer.h

---
+++ QLib/SharedPointer.h
@@ -0,0 +1,109 @@
+#ifndef SHARED_POINTER_H
+#define SHARED_POINTER_H
+
+#include "Pointer.h"
+#include "Exception.h"
+
+namespace QLib
+{
+
+template <typename T>
+class SharedPointer : public Pointer<T>
+{
+protected:
+    int * m_ref;
+    void assign(const SharedPointer<T>& obj);
+public:
+    SharedPointer(T * p = nullptr);
+    SharedPointer(const SharedPointer<T>& obj);
+    SharedPointer<T>& operator = (const SharedPointer<T>& obj);
+    void clear();
+    ~SharedPointer();
+};
+
+template <typename T>
+SharedPointer<T>::SharedPointer(T * p) : m_ref(nullptr)
+{
+    if(p)
+    {
+        this->m_ref =static_cast<int * >(std::malloc(sizeof(int)));
+        if(this->m_ref)
+        {
+            *(this->m_ref) = 1;
+            this->m_pointer = p;
+        }
+        else
+        {
+            THROW_EXCEPTION(NoEnoughMemoryException, "No memory to create SharedPointer.");
+        }
+    }
+}
+
+template <typename T>
+void SharedPointer<T>::assign(const SharedPointer<T>& obj)
+{
+    this->m_ref = obj.m_ref;
+    this->m_pointer = obj.m_pointer;
+    if(this->m_ref)
+    {
+        (*this->m_ref)++;
+    }
+}
+
+template <typename T>
+SharedPointer<T>::SharedPointer(const SharedPointer<T>& obj) : Pointer<T>(nullptr)
+{
+    assign(obj);
+}
+
+template <typename T>
+SharedPointer<T>& SharedPointer<T>::operator = (const SharedPointer<T>& obj)
+{
+    if(this!= &obj)
+    {
+        clear();
+        assign(obj);
+    }
+    return *this;
+}
+
+template <typename T>
+void SharedPointer<T>::clear()
+{
+    T * toDel = this->m_pointer;
+    int * ref = this->m_ref;
+
+    this->m_pointer = nullptr;
+    this->m_ref = nullptr;
+    if(ref)
+    {
+        (*ref)--;
+        if(*ref == 0)
+        {
+            free(ref);
+            delete toDel;
+        }
+    }
+}
+
+template <typename T>
+SharedPointer<T>::~SharedPointer()
+{
+    clear();
+}
+
+template <typename T>
+bool operator == (const SharedPointer<T>&l, const SharedPointer<T>& r)
+{
+    return l.get() == r.get();
+}
+
+template <typename T>
+bool operator != (const SharedPointer<T>&l, const SharedPointer<T>& r)
+{
+    return !(l == r);
+}
+
+}
+#endif
+
