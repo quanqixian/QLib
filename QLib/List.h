e200c6d3fdc25db9ba81649b942c785f9ff32988
Q_uan <quan_qixian@163.com>
2021-03-09 13:13:22 +0800

    add List.h

---
+++ QLib/List.h
@@ -0,0 +1,28 @@
+#ifndef LIST_H
+#define LIST_H
+
+#include "Object.h"
+
+namespace QLib
+{
+
+template <typename T>
+class List : public Object
+{
+protected:
+    List(const List&);
+    List& operator = (const List&);
+public:
+    List(){}
+    virtual bool insert(const T & e) = 0;
+    virtual bool insert(int i,const T & e) = 0;
+    virtual bool remove(int i) = 0;
+    virtual bool set(int i,const T & e) = 0;
+    virtual bool get(int i,T & e)const = 0;
+    virtual int length()const = 0;
+    virtual void clear() = 0;
+};
+
+}
+
+#endif 
